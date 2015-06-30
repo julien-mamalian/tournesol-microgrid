3e02daa6e34d6e92a541738e595ab06e80f65dde
boutayna badou <boutayna.badou@gmail.com>
2015-06-30 12:31:03 +0200

    wip

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,28 @@
+class AccountsController < ApplicationController
+  before_action :set_user
+  skip_after_action :verify_authorized
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to account_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :description, :avatar)
+  end
+
+
+  def set_user
+    @user = current_user
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'accounts/show'
+
+  get 'accounts/edit'
+
+  get 'accounts/update'
+
   # root 'articles#index'
   resources :articles
 
   devise_for :users
+
+  resource :account, only: [:show, :edit, :update]
 end
