7e880da4b6d19c8889263ed31d4032ab45d9e58b
julien mamalian <julien.mamalian@gmail.com>
2015-06-30 16:54:56 +0200

    changements in profile

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,8 @@
 #
 #  id                   :integer          not null, primary key
 #  title                :string
-#  content              :string
-#  votes                :integer
+#  content              :text
+#  votes                :integer          default(0)
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
 #  user_id              :integer
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630102848) do
+ActiveRecord::Schema.define(version: 20150630125027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "content"
-    t.integer  "votes"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.text     "content"
+    t.integer  "votes",                default: 0
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "user_id"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
