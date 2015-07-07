b9028537056051debf75dc31eb16878450eb627b
julien mamalian <julien.mamalian@gmail.com>
2015-07-07 17:57:29 +0200

    buttons social media

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150707132513) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +116,6 @@ ActiveRecord::Schema.define(version: 20150707132513) do
     t.string   "requested_rights",       default: "Not sent", null: false
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
