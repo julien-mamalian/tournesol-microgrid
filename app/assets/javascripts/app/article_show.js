53b1c3fcb4a6207cab320b9f8b88624bd02a2197
julien mamalian <julien.mamalian@gmail.com>
2015-07-07 11:02:45 +0200

    final article version

--- app/assets/javascripts/app/article_show.js
+++ app/assets/javascripts/app/article_show.js
@@ -1,33 +1,29 @@
 $(document).ready(function(){
 
-  var pos = $('.navbar').height() + $('.article_show_content').height() - $(window).height()
-
   $('.article_show_writter').css({
-    'height': $(window).height()
-  })
-
-  $('.article_show_writter').css({
-      'position': 'fixed',
+      'position': 'absolute',
       'top': $('.navbar').height()
   });
 
-  $(window).on('scroll', function() {
-    if ($(window).scrollTop()  > pos) {
-        $('.article_show_writter').css({
-            'position': 'absolute',
-            'top': pos
+  var fromtop = $('.navbar').height()
+  var bottom = $('.navbar').height() + $('.article_show_content').height() - $(window).height()
+
+  $(document).scroll(function() {
+    doc = $(this);
+    dist = $(this).scrollTop();
+
+    if (dist <=  bottom) {
+
+        $('.article_show_writter:first').css({
+            'position': 'fixed',
+            'top': $('.navbar').height()
         });
-        $('.article_show_writter').css({
-          'height': $(window).height() - $('.navbar').height() - $('#retour_link').height()
-        })
     } else {
-      $('.article_show_writter').css({
-        'height': $(window).height()
+
+      $('.article_show_writter:last').css({
+        'position': 'absolute',
+        'top': $('.article_show_content').height() - $('.article_show_writter:last').height()
       })
-      $('.article_show_writter').css({
-          'position': 'fixed',
-          'top': $('.navbar').height()
-      });
     }
   });
 });
--- app/assets/javascripts/app/comments.js
+++ app/assets/javascripts/app/comments.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-  $('.comment').on('click', '.edit-comment--link', function(e) {
+  $('#comments').on('click', '.edit-comment--link', function(e) {
     e.preventDefault();
-
-    $(this).parent().children(".edit_comment").show();
+    $(this).parents('.comment').children('.edit_comment').show();
   });
 });
