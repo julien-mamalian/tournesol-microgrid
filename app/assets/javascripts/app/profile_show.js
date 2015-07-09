acbcaa80085910649366ad819efc59b145322611
julien mamalian <julien.mamalian@gmail.com>
2015-07-09 16:57:13 +0200

    profile

---
+++ app/assets/javascripts/app/profile_show.js
@@ -0,0 +1,11 @@
+$(document)ready(function(){
+  // var window_height = $(window).height();
+  // $('.account_menu').css({
+  //   'height': window_height + 70
+  // });
+  // $('.account_contain').css({
+  //   'height': window_height
+  // });
+});
+
+
